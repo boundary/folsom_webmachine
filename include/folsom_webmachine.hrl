fe283ad6f76047ec1103e679f2deeec0cc693cf8
joewilliams <joe@joetify.com>
2011-10-11 19:11:34 -0700

    initial cut of process and socket resources

--- include/folsom_webmachine.hrl
+++ include/folsom_webmachine.hrl
@@ -1,5 +1,7 @@
 -define(DISPATCH, [{["_system"],folsom_webmachine_system_resource,[]},
                   {["_memory"],folsom_webmachine_memory_resource,[]},
                   {["_statistics"],folsom_webmachine_statistics_resource,[]},
+                  {["_process"],folsom_webmachine_process_resource,[]},
+                  {["_socket"],folsom_webmachine_socket_resource,[]},
                   {["_metrics"],folsom_webmachine_metrics_resource,[]},
                   {["_metrics",id],folsom_webmachine_metrics_resource,[]}]).
