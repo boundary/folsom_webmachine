45f40a2c3a0ea4b553a86f74f649b10bf13662f4
joewilliams <williams.joe@gmail.com>
2012-04-18 09:57:34 -0700

    ets and dets info support

--- include/folsom_webmachine.hrl
+++ include/folsom_webmachine.hrl
@@ -1,9 +1,13 @@
--define(DISPATCH, [{["_system"],folsom_webmachine_system_resource,[]},
-                  {["_memory"],folsom_webmachine_memory_resource,[]},
-                  {["_statistics"],folsom_webmachine_statistics_resource,[]},
-                  {["_process"],folsom_webmachine_process_resource,[]},
-                  {["_port"],folsom_webmachine_port_resource,[]},
-                  {["_ping"],folsom_webmachine_ping_resource,[]},
-                  {["_health"], folsom_webmachine_health_resource, []},
-                  {["_metrics"],folsom_webmachine_metrics_resource,[]},
-                  {["_metrics",id],folsom_webmachine_metrics_resource,[]}]).
+-define(DISPATCH, [
+                   {["_system"],folsom_webmachine_system_resource,[]},
+                   {["_memory"],folsom_webmachine_memory_resource,[]},
+                   {["_statistics"],folsom_webmachine_statistics_resource,[]},
+                   {["_process"],folsom_webmachine_process_resource,[]},
+                   {["_port"],folsom_webmachine_port_resource,[]},
+                   {["_ping"],folsom_webmachine_ping_resource,[]},
+                   {["_health"], folsom_webmachine_health_resource, []},
+                   {["_metrics"],folsom_webmachine_metrics_resource,[]},
+                   {["_metrics",id],folsom_webmachine_metrics_resource,[]},
+                   {["_ets"],folsom_webmachine_ets_resource,[]},
+                   {["_dets"],folsom_webmachine_dets_resource,[]}
+                  ]).
